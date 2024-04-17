745ff3e12df144e83188a59f379c6ec521492ca3
André Norman Lang <andrenormanlang@gmail.com>
2024-04-18 00:56:22 +0200

    changed register controller

--- src/controllers/register_controller.ts
+++ src/controllers/register_controller.ts
@@ -1,103 +1,134 @@
-/**
- * Register Controller
- */
 import bcrypt from 'bcrypt'
 import Debug from 'debug'
 import { Request, Response } from 'express'
-import { matchedData,validationResult } from 'express-validator'
-import prisma from '../prisma'
+import { validationResult, matchedData } from 'express-validator'
+import jwt from 'jsonwebtoken'
+import { JwtPayload } from '../types'
+import { createUser, getUserByEmail } from '../services/user_service'
 
-// Create a new debug instance
-const debug = Debug('prisma-foto-api:register')
+const debug = Debug('api: 🧔‍♀️ user_controller')
 
-/**
- * Get all resources
- */
-export const index = async (req: Request, res: Response) => {
-}
-
-/**
- * Get a single resource
- */
-export const show = async (req: Request, res: Response) => {
-}
-
-/**
- * Register a new user
- */
 export const register = async (req: Request, res: Response) => {
-    // Check for any validation errors
 	const validationErrors = validationResult(req)
 	if (!validationErrors.isEmpty()) {
 		return res.status(400).send({
-			status: "fail",
-			data: validationErrors.array(),
+			status: 'fail',
+			data: validationErrors.array()
 		})
-    }
-
-    //Check if a user with the incoming email already exists (other way of doing the handling)
-	//const user = await prisma.user.findUnique()
-
-  
-    // Get only the validated data from the request
-	const validatedData = matchedData(req)
-	console.log("validatedData:", validatedData)
-
-	// Check if a user with the given email already exists
-	const userExists = await prisma.user.findUnique({
-		where: {
-		email: validatedData.email,
-		},
-	});
-	if (userExists) {
-		return res.status(409).send({ // 409 Conflict might be more appropriate than 400 Bad Request
-		status: "fail",
-		data: "User already exists with the given email address.",
-		});
 	}
-
-    // Calculate a hash + salt for the password
-	const hashedPassword = await bcrypt.hash(validatedData.password, Number(process.env.SALT_ROUNDS) || 10)
-	console.log("Hashed password:", hashedPassword)
-
-    // Replace password with hashed password
-	validatedData.password = hashedPassword
-
-    // Store the user in the database
+	const validData = matchedData(req)
+	const hashedPassword = await bcrypt.hash(validData.password, Number(process.env.SALT_ROUNDS) || 10)
+	validData.password = hashedPassword
 	try {
-		const user = await prisma.user.create({
-			data:{
-				email: validatedData.email,
-                password: validatedData.password,
-                first_name:validatedData.first_name,
-				last_name: validatedData.last_name,
-			},
+		const user = await createUser({
+			email: validData.email,
+			password: validData.password,
+			first_name: validData.first_name,
+			last_name: validData.last_name
 		})
-
-        // Respond with 201 Created + status success
-		res.status(201).send({ 
-			status: "success", 
+		res.status(201).send({
+			status: 'success',
 			data: {
-				id: user.id,
-                email: user.email,
-                first_name: user.first_name,
-                last_name: user.last_name,
-            },
-			
+				email: user.email,
+				first_name: user.first_name,
+				last_name: user.last_name
+			}
 		})
 	} catch (err) {
-		return res.status(500).send({ status: "error", message: "Could not create user in database" })
+		return res.status(500).send({
+			status: 'error',
+			message: "Couldn't create user in database"
+		})
 	}
 }
-/**
- * Update a resource
- */
-export const update = async (req: Request, res: Response) => {
+
+export const login = async (req: Request, res: Response) => {
+	const { email, password } = req.body
+	const user = await getUserByEmail(email)
+	if (!user) {
+		return res.status(401).send({
+			status: 'fail',
+			message: "Authorization required"
+		})
+	}
+	const isPasswordCorrect = await bcrypt.compare(password, user.password)
+	if (!isPasswordCorrect) {
+		return res.status(401).send({
+			status: 'fail',
+			message: "Authorization required"
+		})
+	}
+	const payload: JwtPayload = {
+		sub: user.id,
+		email: user.email
+	}
+	if (!process.env.ACCESS_TOKEN_SECRET) {
+		return res.status(500).send({
+			status: 'error',
+			message: "No access token secret defined"
+		})
+	}
+	const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: process.env.ACCESS_TOKEN_LIFETIME || '4h'
+	})
+	if (!process.env.REFRESH_TOKEN_SECRET) {
+		return res.status(500).send({
+			status: 'error',
+			message: "No refresh token secret defined"
+		})
+	}
+	const refresh_token = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+		expiresIn: process.env.REFRESH_TOKEN_LIFETIME || '1d'
+	})
+	res.send({
+		status: 'success',
+		data: {
+			access_token,
+			refresh_token
+		}
+	})
 }
 
-
-/**
- * Delete a resource
- */
-export const destroy = async (req: Request, res: Response) => {
+export const refresh = (req: Request, res: Response) => {
+	if (!req.headers.authorization) {
+		debug("Authorization header missing")
+		return res.status(401).send({
+			status: 'fail',
+			data: "Authorization required"
+		})
+	}
+	const [authSchema, token] = req.headers.authorization.split(' ')
+	if (authSchema.toLowerCase() !== "bearer") {
+		debug("Authorization schema isn't Bearer")
+		return res.status(401).send({
+			status: 'fail',
+			data: "Authorization required"
+		})
+	}
+	try {
+		const payload = (jwt.verify(token, process.env.REFRESH_TOKEN_SECRET || '') as unknown) as JwtPayload
+		delete payload.iat
+		delete payload.exp
+		if (!process.env.ACCESS_TOKEN_SECRET) {
+			return res.status(500).send({
+				status: 'error',
+				message: "No access token secret defined"
+			})
+		}
+		const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+			expiresIn: process.env.ACCESS_TOKEN_LIFETIME || '4h'
+		})
+		res.send({
+			status: 'success',
+			data: {
+				access_token
+			}
+		})
+	} catch (err) {
+		debug("Token failed verification", err)
+		return res.status(401).send({
+			status: 'fail',
+			data: "Authorization required"
+		})
+	}
 }
